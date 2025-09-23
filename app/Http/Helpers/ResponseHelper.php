5f9cfca46e32818489877949bfc15e377801b2be
berkebzlk <berkebozlak10@hotmail.com>
2025-09-23 22:55:31 +0300

    ResponseHelper created

---
+++ app/Http/Helpers/ResponseHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Helpers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class ResponseHelper
+{
+    public static function success(
+        mixed $data = null,
+        int $statusCode = Response::HTTP_OK,
+        ?string $message = null
+    ): JsonResponse {
+        $response = [
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ];
+
+        return response()->json($response, $statusCode);
+    }
+}
