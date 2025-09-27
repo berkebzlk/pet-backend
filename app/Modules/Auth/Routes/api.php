6cfd2607cf64f7375e550465f56f440b5f5c1eee
berkebzlk <berkebozlak10@hotmail.com>
2025-09-27 20:58:28 +0300

    role api routed created, auth api routes was missing; added

--- app/Modules/Auth/Routes/api.php
+++ app/Modules/Auth/Routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Modules\Auth\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/me', [AuthController::class, 'me']);
 });
 
 
---
+++ app/Modules/Role/Routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Modules\Role\Controllers\RoleController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('role')->group(function () {
+    Route::post('/', [RoleController::class, 'store']);
+    Route::get('/', [RoleController::class, 'index']);
+    Route::get('/{id}', [RoleController::class, 'show']);
+    Route::put('/{id}', [RoleController::class, 'update']);
+    Route::delete('/{id}', [RoleController::class, 'delete']);
+});
+
+
