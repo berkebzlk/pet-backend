028faab61b1dfd25e514db289c470fa29273fa19
berkebzlk <berkebozlak10@hotmail.com>
2025-10-11 23:28:41 +0300

    names added to routes

--- app/Modules/Auth/Routes/api.php
+++ app/Modules/Auth/Routes/api.php
@@ -4,11 +4,11 @@ use App\Modules\Auth\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::post('/me', [AuthController::class, 'me']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->name('me');
 });
 
 
